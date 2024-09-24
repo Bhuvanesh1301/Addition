0c0168b2a6c9c1591c823a0521d55a7dd37609d9
Bhuvanesh1301 <111751834+Bhuvanesh1301@users.noreply.github.com>
2024-09-24 11:46:01 +0530

    commit

---
+++ demo/src/test/java/com/example/AdditionTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+public class AdditionTest{
+    @Test
+    public void testAdd()
+    {
+        Addition addition = new Addition();
+        int result = addition.add(5,3);
+        assertEquals("trst for add",8,result)
+    }
+}
